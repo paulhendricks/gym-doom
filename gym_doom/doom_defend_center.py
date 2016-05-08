5ad1b60cd3b6925b0908c6fc6fc5900aefee233a
Greg Brockman <gdb@gregbrockman.com>
2016-05-07 23:08:13 -0700

    Fix remaining HighLow imports

--- gym_doom/doom_defend_center.py
+++ gym_doom/doom_defend_center.py
@@ -2,12 +2,9 @@ import logging
 import os
 from gym import error, spaces
 import numpy as np
-from gym_doom import doom_env
+from gym_doom import doom_env, spaces as doom_spaces
 
-try:
-    from doom_py import DoomGame, Mode, Button, GameVariable, ScreenFormat, ScreenResolution, Loader
-except ImportError as e:
-    raise error.DependencyNotInstalled("{}. (HINT: you can install Doom dependencies with 'pip install gym[doom].)'".format(e))
+from doom_py import DoomGame, Mode, Button, GameVariable, ScreenFormat, ScreenResolution, Loader
 
 logger = logging.getLogger(__name__)
 
@@ -50,7 +47,7 @@ class DoomDefendCenterEnv(doom_env.DoomEnv):
         self.game.set_doom_scenario_path(self.loader.get_scenario_path('defend_the_center.wad'))
         self.screen_height = 480                    # Must match .cfg file
         self.screen_width = 640                     # Must match .cfg file
-        self.action_space = spaces.HighLow(np.matrix([[0, 1, 0]] * 36 + [[0, 10, 0]] * 5))
+        self.action_space = doom_spaces.HighLow(np.matrix([[0, 1, 0]] * 36 + [[0, 10, 0]] * 5))
         self.observation_space = spaces.Box(low=0, high=255, shape=(self.screen_height, self.screen_width, 3))
         self.allowed_actions = [0, 13, 14]          # Must match order in .cfg file
         self.game.set_window_visible(False)
